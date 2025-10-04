27b4032f619672b07930da37ded6071d1300bdfe
HarshJaiswal <harshjaiswal497@gmail.com>
2025-10-04 20:43:23 +0530

    implemented notification service simulation

---
+++ src/main/java/com/harsh/store/NotificationService.java
@@ -0,0 +1,8 @@
+package com.harsh.store;
+
+import org.springframework.stereotype.Service;
+
+
+public interface NotificationService {
+    void send(String message);
+}
--- src/main/java/com/harsh/store/StoreApplication.java
+++ src/main/java/com/harsh/store/StoreApplication.java
@@ -2,12 +2,19 @@ package com.harsh.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
-	}
+		// Start Spring Boot application and get ApplicationContext
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
+		// Get the NotificationManager bean from the context
+		NotificationManager notificationManager = context.getBean(NotificationManager.class);
+
+		// Call the sendNotification method
+		notificationManager.sendNotification("Hello from Spring Boot!");
+	}
 }
